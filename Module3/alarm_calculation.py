4d3d0400a1aa7e8185f78afbca05232d6f74dc57
Melissa Hidalgo <melissa.hidalgo@csuglobal.edu>
2024-05-26 12:40:04 -0700

    Finished Part 2

--- Module3/alarm_calculation.py
+++ Module3/alarm_calculation.py
@@ -6,3 +6,34 @@
 # Your program should output what the time will be on a 24-hour clock when the alarm goes off.
 #
 # By: Melissa Hidalgo 352865
+
+# Function to check if the user entered hour is a valid input
+def validate_hour(string):
+    if(not string.isdigit()):
+        return False
+    if(int(string) > 23 or int(string) < 0):
+        return False
+    
+    return True
+
+def set_alarm(current_hour, hours_from_now):
+    hours_passed = current_hour + hours_from_now
+    return hours_passed % 24
+
+hour_str = input('Enter the current hour between 0 and 23: ')
+
+# Make sure entry is valid
+while not validate_hour(hour_str):
+    hour_str = input('Invalid entry. Please enter the hour as an integer between 0 and 23: ')
+
+hour = int(hour_str)
+
+alarm_hours_str = input('Enter the number of hours from now that you would like to set an alarm for: ')
+
+while not alarm_hours_str.isdigit():
+    alarm_hours_str = input('Invalid entry. Please enter a whole number')
+
+alarm_hours = int(alarm_hours_str)
+
+time_on_clock = set_alarm(hour, alarm_hours)
+print('It will be', time_on_clock, 'when your alarm goes off')
